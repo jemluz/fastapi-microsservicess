7232edaf31dfc91f5f684cd1ced44f887412f933
Jemima Luz <contatojemima@gmail.com>
2022-04-20 14:57:51 -0300

    create payment service

---
+++ payment/main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+import requests
+import time
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+redis = get_redis_connection(
+    host="redis-16907.c289.us-west-1-2.ec2.cloud.redislabs.com",
+    port=16907,
+    password="Q16Qv3QGjlPV765fghwpt9kthEJZJjh7",
+)
+
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str
+
+    class Meta:
+        database = redis
+
+@app.get('/orders/{pk}')
+def get(pk: str):
+    return Order.get(pk)
+
+@app.post('/orders')
+async def create(request: Request, background_tasks: BackgroundTasks): # id, quantity
+    body = await request.json()
+
+    req = requests.get('http://localhost:8000/products/%s' % body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id = body['id'],
+        price = product['price'],
+        fee = product['price'] * .2,
+        total = product['price'] * 1.2,
+        quantity = product['quantity'],
+        status = 'pending'
+    )
+
+    order.save()
+    background_tasks.add_task(order_completed, order)
+
+    return order
+
+def order_completed(order: Order):
+    time.sleep(5)
+    order.status = 'completed'
+    order.save()
+    redis.xadd('order_completed', order.dict(), '*')
